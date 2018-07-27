ecd2c06243519e5b8d6faf10cedb6cdb3bb14996
RomaMehta <dr.roma.nehete@gmail.com>
2018-07-27 02:55:22 +0100

    modifications

--- HomeWork21stJuly/src/test/java/TrialCucumber/Hooks.java
+++ HomeWork21stJuly/src/test/java/TrialCucumber/Hooks.java
@@ -3,6 +3,12 @@ package TrialCucumber;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
+
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class Hooks extends BasePage {
@@ -18,7 +24,31 @@ public class Hooks extends BasePage {
 
     }
     @After
+    public void screenShot(ITestResult result){
+        //here we are comparing if the test fails, only then it will enter the if condition loop
+       if  (ITestResult.FAILURE == result.getStatus()) {
+            try{
+                // Create reference of TakesScreenshot
+                TakesScreenshot ts = (TakesScreenshot) driver;
+
+                // Calling the method to take the screenshot
+                File source = ts.getScreenshotAs(OutputType.FILE);
+
+                // Copy files to specific location here it will save all screenshot in our project home directory and
+                // result.getName() will return name of test case so that screenshot name will be same
+                FileUtils.copyFile(source, new File("src\\test\\Resources\\Screenshots\\" + result.getName() + System.currentTimeMillis() + ".png"));
+
+                System.out.println("ScreenShot Taken");
+            } catch (Exception e) {
+
+                System.out.println("Exception while taking screenshot" + e.getMessage());
+            }
+        }
+    }
+
+    @After
     public void toCloseBrowser(){
       driver.quit();
     }
 }
+
